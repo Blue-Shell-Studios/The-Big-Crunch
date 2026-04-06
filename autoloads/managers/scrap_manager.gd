a38f5d169ce097d268161018ec76e9db34b20ead
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-04-07 00:10:20 +0800

    feat: add scrap manager

---
+++ autoloads/managers/scrap_manager.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const SCRAP_SCENE = preload("uid://boj27uu22mvnt")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SignalBus.spawn_scraps.connect(spawn_scraps)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func spawn_scraps(origin: Vector2, spread: float, amount: int) -> void:
+	for i in range(amount):
+		var scrap = SCRAP_SCENE.instantiate()
+		add_child(scrap)
+		
+		scrap.position = origin + Vector2(randf_range(-spread, spread), randf_range(-spread, spread))
