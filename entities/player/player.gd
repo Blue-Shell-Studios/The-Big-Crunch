53a2ce3859ab62d72f532536efa226102a5cb211
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-04-07 14:28:29 +0800

    feat: integrate varying player body

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,11 +1,24 @@
 extends RigidBody2D
 
+enum Stage {SMALL, MEDIUM, LARGE, MASSIVE}
+
+const MASSIVE_PLAYER = preload("uid://cdsarwfuk2hrn")
+const LARGE_PLAYER = preload("uid://dwmprv3fl0yy5")
+const MEDIUM_PLAYER = preload("uid://cv8um7ekrmdgh")
+
 @export_group("Movement Settings")
 @export var sensitivity: float = 0.4
-@export var max_speed: float = 500.0
+@export var max_speed: float = 800.0
 @export var friction: float = 0.92 # 1.0 = No friction, 0.0 = Instant stop
 @export var rotation_speed: float = 10.0
 
+@onready var vessel: Area2D = $SmallPlayer
+@onready var camera: Camera2D = $Camera2D
+
+var is_moving := false
+var stage := Stage.SMALL
+
+var health := 150
 var mouse_delta = Vector2.ZERO
 
 func _ready():
@@ -36,7 +49,41 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 
 	# Prevent Physics Drift
 	state.angular_velocity = 0
+	
+func upgrade() -> void:
+	if stage == Stage.MASSIVE: return
+	
+	stage += 1
+	
+	max_speed *= 1.2
+	
+	vessel.queue_free()
+	match stage:
+		Stage.MEDIUM:
+			health = 600
+			var medium_vessel = MEDIUM_PLAYER.instantiate()
+			add_child(medium_vessel)
+			
+			vessel = medium_vessel
+			camera.zoom = Vector2(0.8, 0.8)
+		Stage.LARGE:
+			health = 3900
+			var large_vessel = LARGE_PLAYER.instantiate()
+			add_child(large_vessel)
+			
+			vessel = large_vessel
+			camera.zoom = Vector2(0.7, 0.7)
+		Stage.MASSIVE:
+			health = 10000
+			var massive_vessel = MASSIVE_PLAYER.instantiate()
+			add_child(massive_vessel)
+			
+			vessel = massive_vessel
+			camera.zoom = Vector2(0.5, 0.5)
 
-func _process(_delta):
+func _process(_delta) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if Input.is_action_just_pressed("attack"):
+		vessel.attack()
