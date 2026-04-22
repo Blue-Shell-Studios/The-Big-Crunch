6a6527a64d9f9d586b31649d73599e2e6ac287a9
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-04-22 23:54:21 +0800

    refactor: change scrap to react to collector module

--- entities/scrap/scrap.gd
+++ entities/scrap/scrap.gd
@@ -1,21 +1,29 @@
-extends Area2D
+class_name Scrap extends Area2D
 
+var value : int = 1
+var collector_module : CollectorModule
 var target_position : Vector2
-var sucking_force : int
+var magnetic_force : int
 
 func _process(delta: float) -> void:
-	if target_position == Vector2.ZERO: return
+	if collector_module == null: return
 	var direction := global_position.direction_to(target_position)
 	
-	global_position += direction * sucking_force * delta
+	global_position += direction * magnetic_force * delta
 	
-	if global_position.distance_to(target_position) < 5.0:
+	if global_position.distance_to(target_position) < 10.0:
+		var ship := collector_module.get_parent()
+		if ship is PlayerVessel:
+			var player: Player = ship.get_parent()
+			player.exp += value
+			
 		queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("net"):
-		var temp: int = area.get_parent().GATHERING_POWER
-		if temp <= sucking_force: return 
+	if area.is_in_group("collector_module"):
+		var module: CollectorModule = area
+		if module.power <= magnetic_force: return 
 		
-		target_position = area.global_position
-		sucking_force = temp
+		collector_module = module
+		target_position = module.global_position
+		magnetic_force = module.power
