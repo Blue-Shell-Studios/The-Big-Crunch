505557d0d819a778ef4d797d54248ba52ebc25a9
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-04-26 03:45:40 +0800

    refactor: change movement from "move to direction" to steer and throttle

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -5,8 +5,15 @@ enum Stage {SMALL, MEDIUM, LARGE, MASSIVE}
 const MASSIVE_PLAYER = preload("uid://cdsarwfuk2hrn")
 const LARGE_PLAYER = preload("uid://dwmprv3fl0yy5")
 const MEDIUM_PLAYER = preload("uid://cv8um7ekrmdgh")
+const CAMERA_FORWARD_SCREEN_OFFSET = PI / 2.0
 
-@export var rotation_speed: float = 10.0
+@export var rotation_speed: float = 1
+@export var angular_acceleration: float = 3.5
+@export_range(0.0, 1.0) var turn_control_at_max_speed: float = 0.35
+@export var drift_drag: float = 35.0
+@export var reverse_thrust_multiplier: float = 0.55
+@export var reverse_speed_threshold: float = 45.0
+@export var camera_rotation_follow_speed: float = 3.5
 
 @onready var vessel: PlayerVessel = $SmallPlayer
 @onready var camera: Camera2D = $Camera2D
@@ -26,6 +33,8 @@ var exp: int :
 		SignalBus.update_exp.emit(exp, max_exp)
 
 func _ready():
+	camera.global_position = global_position
+	camera.global_rotation = get_camera_target_rotation()
 	max_exp = vessel.exp_requirement
 	exp = 0
 
@@ -42,40 +51,38 @@ func transition_camera_zoom(target_zoom: Vector2) -> void:
 	camera_zoom_tween.set_ease(Tween.EASE_OUT)
 	camera_zoom_tween.tween_property(camera, "zoom", target_zoom, 0.8)
 
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	var net_force : Vector2 = Vector2.ZERO
-	var stop_threshold : float = 10
+func get_camera_target_rotation() -> float:
+	return global_rotation + CAMERA_FORWARD_SCREEN_OFFSET
 
-	if direction == Vector2.ZERO:
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var net_force : Vector2 = Vector2.ZERO
+	var turn_direction := Input.get_axis("ui_left", "ui_right")
+	var throttle := Input.get_axis("ui_down", "ui_up")
+	var speed_ratio: float = clamp(state.linear_velocity.length() / vessel.max_speed, 0.0, 1.0)
+	var turn_control: float = lerp(1.0, turn_control_at_max_speed, speed_ratio)
+	var target_angular_velocity := turn_direction * rotation_speed * turn_control
+	var angular_step := angular_acceleration * state.step
+
+	state.angular_velocity = move_toward(state.angular_velocity, target_angular_velocity, angular_step)
+	var forward := Vector2.RIGHT.rotated(global_rotation)
+
+	if throttle == 0.0:
 		vessel.state = PlayerVessel.State.IDLE
-		
-		if state.linear_velocity.length() < stop_threshold:
-			# hard stop
-			state.linear_velocity = Vector2.ZERO
-			net_force = Vector2.ZERO
-		else:
-			# apply braking force
-			net_force = -state.linear_velocity.normalized() * vessel.max_braking_force
+		if state.linear_velocity != Vector2.ZERO:
+			net_force = -state.linear_velocity.normalized() * drift_drag
 	else:
 		vessel.state = PlayerVessel.State.MOVING
-		# accelerate
-		net_force = direction * vessel.engine_power
+		if throttle > 0.0:
+			net_force = forward * vessel.engine_power * throttle
+		elif state.linear_velocity.length() > reverse_speed_threshold:
+			net_force = -state.linear_velocity.normalized() * vessel.max_braking_force
+		else:
+			net_force = -forward * vessel.engine_power * reverse_thrust_multiplier * abs(throttle)
 
 	state.apply_central_force(net_force)
 	
-	# clamp velocity
 	if state.linear_velocity.length() > vessel.max_speed:
 		state.linear_velocity = state.linear_velocity.normalized() * vessel.max_speed
-
-	# Rotation
-	if state.linear_velocity.length() > 20:
-		var target_angle = state.linear_velocity.angle()
-		global_rotation = lerp_angle(global_rotation, target_angle, rotation_speed * state.step)
-
-	# Prevent Physics Drift
-	state.angular_velocity = 0	
 	
 func upgrade() -> void:
 	if stage == Stage.MASSIVE: return
@@ -106,6 +113,9 @@ func upgrade() -> void:
 			sync_progress_to_vessel()
 			transition_camera_zoom(Vector2(0.5, 0.5))
 
-func _process(_delta) -> void:
-	if Input.is_action_just_pressed("attack"):
+func _process(delta: float) -> void:
+	camera.global_position = global_position
+	camera.global_rotation = lerp_angle(camera.global_rotation, get_camera_target_rotation(), camera_rotation_follow_speed * delta)
+
+	if Input.is_action_pressed("attack"):
 		vessel.attack()
